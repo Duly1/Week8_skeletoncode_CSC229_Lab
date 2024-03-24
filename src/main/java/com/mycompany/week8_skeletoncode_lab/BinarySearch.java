a1b02c3de894b291858d757fbdf7d20bd38bbfd6
Duly1 <157521117+Duly1@users.noreply.github.com>
2024-03-24 15:47:22 -0400

    Update BinarySearch.java
    The todo section

--- src/main/java/com/mycompany/week8_skeletoncode_lab/BinarySearch.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/BinarySearch.java
@@ -30,4 +30,8 @@ public class BinarySearch {
     //ToDo 2: Call the above method and test the algorithm  
     // provide time and space analysis 
 
+     int index = runBinarySearchIteratively(sortedArray, key, low, high); 
+    // space is halved in each loop so the time complexity of binary searcg is O(logn)
+    // space complexity is O(1) meaning its constant space regardless of the size of the input array. Due to the algorithm not using any data strucutres where size scales are used. 
+
 }
